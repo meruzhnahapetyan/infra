e22c5c98b9e41f9182b401a0a4a2187d7f596009
Meruzh Nahapetyan <meruzhnahapetyan@gmail.com>
2023-05-02 14:24:27 +0400

    changes

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 
 module "vpc-dev" {
-    source = "../terraform-module-infra"
-    cidr_block = "10.0.0.0/16"
+    source = "../../terraform-module-infra"
+    cidr_block = "10.0.0.0/17"
     public_subnet_cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24"]
     private_subnet_cidr_blocks = ["10.0.3.0/24", "10.0.4.0/24"]
     availability_zones = ["us-west-2a", "us-west-2b"]
