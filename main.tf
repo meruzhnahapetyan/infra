be883f3c644d6f2bb978a851589f18db20da372e
Meruzh Nahapetyan <meruzhnahapetyan@gmail.com>
2023-05-02 14:09:16 +0400

    initial commit

---
+++ main.tf
@@ -0,0 +1,8 @@
+
+module "vpc-dev" {
+    source = "../terraform-module-infra"
+    cidr_block = "10.0.0.0/16"
+    public_subnet_cidr_blocks = ["10.0.1.0/24", "10.0.2.0/24"]
+    private_subnet_cidr_blocks = ["10.0.3.0/24", "10.0.4.0/24"]
+    availability_zones = ["us-west-2a", "us-west-2b"]
+}
